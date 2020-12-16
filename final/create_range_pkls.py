cc0c94e1b010354a6605816d96e1ef9e48809b0f
Elizabeth Vandegriff <elizabeth.vandegriff@gmail.com>
2020-12-15 19:11:23 -0600

    comments added to create_range_pkls

--- final/create_range_pkls.py
+++ final/create_range_pkls.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python3
 """
-Simple script for 
+Simple script for looping through 'calc_ae.py' to create pkls for a range of 
+station values.
+Output folder will have start, stop, and step values in folder name.
 """
 
+# only need to import one thing!
 import os
 
+# still need to set ae_calc.py to debug if you want any printed statements
 debug = True
 
-start = 10
+# define the start, astop, and step integers to loop through
+start = 12
 stop = 361
 step = 36
 
+# separate script runs for debug and full versions
 if debug:
-    for rate in range(start, stop, step): 
+    # loop through from start to stop ints, by step, and run calc_ae.py
+    for rate in range(start, stop, step):
+        # use os. system to run the string below in the command line
         os.system(f'python calc_ae.py mag_files_debug/ ae_pkls_debug_{start}_{stop}_{step}/ -r {rate}' )
 else:
-    for rate in range(start, stop, step): 
+    # loop through from start to stop ints, by step, and run calc_ae.py
+    for rate in range(start, stop, step):
+        # use os. system to run the string below in the command line
         os.system(f'python calc_ae.py mag_files/ ae_pkls_{start}_{stop}_{step}/ -r {rate}')
