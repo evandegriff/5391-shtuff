dde7fce97df6b275c7f7cca878a63ebe2752ca79
Elizabeth Vandegriff <elizabeth.vandegriff@gmail.com>
2020-12-15 21:20:25 -0600

    added comments to calc_ae.py

--- final/calc_ae.py
+++ final/calc_ae.py
@@ -13,81 +13,107 @@ parser = ArgumentParser(description=__doc__)
 # magdir () is a required arg
 parser.add_argument('magdir', help='File directory where mag files are.',
                     type=str)
-# 
-parser.add_argument('outdir', help='File directory for pkl output files.'+
-                    'Defaults to /ae_pkls.', type=str,  default='ae_pkls_debug/')
+# outdir is where the resulting pkls will be saved
+parser.add_argument('outdir', help='Directory name for output pkls.'+
+                    'Does not need to already exist', type=str)
+# u is the upper latitude limit for the AE calculation
 parser.add_argument('-u', '--u', help='Value of upper latitude limit. '+
                     'Defaults to 70 degrees.', type=int, default=70)
+# l is the lower latitude limit for the AE calculation
 parser.add_argument('-l', '--l', help='Value of lower latitude limit. '+
                     'Defaults to 65 degrees.', type=int, default=65)
-parser.add_argument('-r', '--r', help='Value of magnetometer resample rate.'+
-                    'Defaults to 360 (no resample).', type=int, default=360)
-parser.add_argument('-o', '--o', help='File directory for pkl output files.'+
-                    'Defaults to /ae_pkls.', type=str, default='ae_pkls/')
+# r is the resample rate - how many magnetometers to use for AE calculation
+parser.add_argument('-r', '--r', help='Number of mags used at each latitude.'+
+                    'Defaults to 360 (all).', type=int, default=360)
 
-# Get args from caller, collect arguments into a convenient object:
+
+# collect args together
 args = parser.parse_args()
 
-import spacepy.pybats.bats as pbs
-import spacepy.pybats as pb
-import numpy as np
-import os
-from glob import glob
-from pickle import load, dump
-from scipy.signal import resample
+# now start the rest of the file
+# import necessary packages
+import spacepy.pybats.bats as pbs # allows us to open the magnetometer files
+import numpy as np # to get min/max of arrays
+import os # to create output folders
+from glob import glob # to assemble mag files
+from pickle import load, dump # to store calculated AE
+from scipy.signal import resample #to specify how many mags to use
 
+
+# set a boolean to limit/add functionality during debugging/testing
+# makes it easier to make small changes without re-running entire fileset
 debug = True
 
+# name variables from the arguments
 output_folder = args.outdir
 upper_lat = args.u
 lower_lat = args.l
 resample_rate = args.r
 
+# if in debugging mode, print latitude limits
 if debug: print(upper_lat, lower_lat)
 
+# check if desired output folder exists, if not, create it
 if not os.path.exists(output_folder): os.makedirs(output_folder)
 
+# use glob to collect all magnetometer files, sort by filename
 mag_files = sorted(glob(f'./{args.magdir}/mag*.out'))
 
+# looping through thousands of files takes a loooooong time
+# for the debug case, specify small subset of magnetometer files to use
 if debug: mag_files = [f'./{args.magdir}/mag_grid_e20170906-180250.out',
                        f'./{args.magdir}/mag_grid_e20170907-210350.out',
                        f'./{args.magdir}/mag_grid_e20170908-170530.out']
 
+# create dict with empty arrays for calculated values and time
 ae_dict = {'epochs':[], 
            'al': [], 
            'au': [], 
            'ae': [], 
            'ao': []}
 
+# loop through all mag files and calculate AL, AU, AE, and AO
 for mag_file in mag_files:
-    epoch = pb.parse_filename_time(mag_file)[2]
-    if debug: print(epoch)
+    # use spacepy to open mag file
     mag = pbs.MagGridFile(mag_file)
+    # get file time from attributes
+    epoch =  mag.attrs['time']
+    # more debugging print statements
+    if debug: print(epoch)
+    # get dBn array for selected latitudes only
     dBn = mag['dBn'][:,lower_lat:upper_lat+1]
+    # resample data to specific number of magnetometers
     dBn = resample(dBn, resample_rate)
+    # more debugging print statements
     if debug: print(np.shape(dBn))
-    dBn_min = np.amin(dBn)
-    dBn_max = np.amax(dBn)
-    al = dBn_min
-    au = dBn_max
+    # set AL to be min of resampled array
+    al = np.amin(dBn)
+    # set AU to max of resampled array
+    au = np.amax(dBn)
+    # calculate ae as the difference between AU and AL
     ae = au - al
     # (AU+AL)/2 defines the AO index
     ao = (au + al)/2
+    # more debugging print statements
     if debug:
         print(f'epoch = {epoch}')
         print(f'al = {al}')
         print(f'au = {au}')
         print(f'ae = {ae}')
         print(f'ao = {ao}')
+    # add all calculated values to respective arrays in ea_dict
     ae_dict['epochs'].append(epoch)
     ae_dict['al'].append(al)
     ae_dict['au'].append(au)
     ae_dict['ae'].append(ae)
     ae_dict['ao'].append(ao)
 
+# more debugging print statements
 if debug: print(ae_dict)
 
+# save ae_dict to pkl, different filenames/locations for debug version
 if debug:
+    # use resample rate in filename to distinguish it from others
     with open(f'./{output_folder}/ae_dict{resample_rate:03}_debug.pkl', 'wb') as f: dump(ae_dict, f)
 else:
     with open(f'./{output_folder}/ae_dict{resample_rate:03}.pkl', 'wb') as f: dump(ae_dict, f)
